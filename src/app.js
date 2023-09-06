a6c7ed3c4a3fa614868872592abac6d4c8257c4f
Verahvilasboas <136754249+Verahvilasboas@users.noreply.github.com>
2023-09-06 19:54:06 +0100

    add javascript

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
-let apiKey = "b653b400b8db20ab830606fd7adac678";
-let apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}`;
-console.log(apiUrl)
+function getForecast(coordinates) {
+  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+console.log(apiUrl);
