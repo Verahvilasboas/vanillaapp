36038f3e621372625f1f395f17ab45cb14774cd1
Verahvilasboas <136754249+Verahvilasboas@users.noreply.github.com>
2023-09-06 19:45:26 +0100

    css style changed

--- src/app.js
+++ src/app.js
@@ -1,1 +1,3 @@
-
+let apiKey = "b653b400b8db20ab830606fd7adac678";
+let apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}`;
+console.log(apiUrl)
