b2cbe9e63b1613284935270a5ee46b2cf100bbdd
Verahvilasboas <136754249+Verahvilasboas@users.noreply.github.com>
2023-09-08 09:17:39 +0100

    added more style

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 
-  let apiKey = "cb653b400b8db20ab830606fd7adac678";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}&units=metric`;
+  let apiKey = "9e0fb79c2f66d0cd0dcf06710976a873";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayForecast);
 
 console.log(apiUrl);
