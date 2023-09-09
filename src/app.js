14842c340bb8a55b8e505033df5e4befbb8f1ccf
Verahvilasboas <136754249+Verahvilasboas@users.noreply.github.com>
2023-09-09 01:41:11 +0100

    api key not working

--- src/app.js
+++ src/app.js
@@ -1,32 +1,29 @@
 
   function displayTemperature(response){
     let temperatureElement = document.querySelector("#temperature");
-    let cityElement = document.querySelector("city");
-    let description = document.querySelector("description");
+    let cityElement = document.querySelector("#city");
+    let description = document.querySelector("#description");
+    let humidityElement= document.querySelector("#humidity");
+    let windElement=document.querySelector("#wind");
     temperatureElement.innerHTML = Math.round
     (response.data.main.temp);
     cityElement.innerHTML=response.data.name;
     descriptionElement.innerHTML=response.data.weather[0].description.to;
+    humidityElement.innerHTML=response.data.main.humidity;
+     windElement.innerHTML = response.data.wind.speed;
   }
   
 
     //make api call
-  let apiKey = "49b631c45785fe73d2a88477803dea22";
+  let apiKey = "9e0fb79c2f66d0cd0dcf06710976a873";
   let city = document.querySelector("#city-input").value;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
+  q=New York&appid=${apiKey}&units=metric`; 
+  
+  
   axios.get(apiUrl).then(displayTemperature);
   console.log(apiUrl);
 
   
-  
-  
-  
-  function searchLocation(position) {
- let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
- let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
- axios.get(apiUrl).then(displayTemperature);
-  }
-
-  console.log(apiUrl);
 
 
