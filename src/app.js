0dc134053c66f6c7e0423408c792659ccb7021a5
Verahvilasboas <136754249+Verahvilasboas@users.noreply.github.com>
2023-09-09 19:53:00 +0100

    problemas with api i used last code

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-  function formatDate(timestamp) {
-//calculate the date 
-let date = new date (timestamp);
-let hours = date.getHours();
-if (hours < 10) {
-hours = '0${hours}';
-}
-let minutes = date.getMinutes();
-if (minutes < 10) {
-  minutes = '0${minutes}';
-}
+  let dayIndex = date.getDay();
   let days = [
     "Sunday",
     "Monday",
@@ -19,37 +18,27 @@ if (minutes < 10) {
     "Friday",
     "Saturday",
   ];
-let day = days[date.getDay()];
-return '${day} &{hours}:${minutes}';
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
 }
-  
-  
-  function displayTemperature(response){
 
-    let temperatureElement = document.querySelector("#temperature");
-    let cityElement = document.querySelector("#city");
-    let descriptionElement = document.querySelector("#description");
-    let humidityElement= document.querySelector("#humidity");
-    let windElement=document.querySelector("#wind");
-    let dateElement=document.querySelector("#date");
-    let iconElement=document.querySelector("#icon");
+//second step
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
 
-    temperatureElement.innerHTML = Math.round
-    (response.data.main.temp);
-    cityElement.innerHTML=response.data.name;
-    descriptionElement.innerHTML=response.data.weather[0].description.to;
-    humidityElement.innerHTML=response.data.main.humidity;
-     windElement.innerHTML = Math.round(response.data.wind.speed);
-     dateElement.innerHTML = formatDate(response.data.dt * 1000);
-     iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-  iconElement.setAttribute("alt", response.data.weather[0].description); }
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
 
-    //make api call
- function search(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value
- } 
-
+//fisrt step
 function search(event) {
   event.preventDefault();
   //make api call
@@ -64,9 +53,46 @@ function search(event) {
   cityElement.innerHTML = cityInput.value;
 }
 
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
 
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 64;
+}
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+//current
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 
+// Bonus Feature
 
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+function searchLocation(position) {
+  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+// bonue feature week 5
+let currentButton = document.querySelector("#current-button");
+currentButton.addEventListener("click", getCurrentLocation);
