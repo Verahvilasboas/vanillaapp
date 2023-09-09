cea4cf8178113cc22ccc38a65120688aefe5a56d
Verahvilasboas <136754249+Verahvilasboas@users.noreply.github.com>
2023-09-09 17:47:08 +0100

    js

--- src/app.js
+++ src/app.js
@@ -31,7 +31,8 @@ return '${day} &{hours}:${minutes}';
     let description = document.querySelector("#description");
     let humidityElement= document.querySelector("#humidity");
     let windElement=document.querySelector("#wind");
-    let dateElement=document.querySelector("#date")
+    let dateElement=document.querySelector("#date");
+    let iconElement=document.querySelector("#icon");
     temperatureElement.innerHTML = Math.round
     (response.data.main.temp);
     cityElement.innerHTML=response.data.name;
@@ -39,14 +40,13 @@ return '${day} &{hours}:${minutes}';
     humidityElement.innerHTML=response.data.main.humidity;
      windElement.innerHTML = Math.round(response.data.wind.speed);
      dateElement.innerHTML = formatDate(response.data.dt * 1000);
-
-  }
-  
+     iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description); }
 
     //make api call
   function getForecast(coordinates) {
     let apiKey = "9e0fb79c2f66d0cd0dcf06710976a873";
-    let city = document.querySelector("#city-input").value;
+    let city = "New York"
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(displayTemperature);
