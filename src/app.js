e3ff08983c0f66ac51410d953312eea050aaee2e
Verahvilasboas <136754249+Verahvilasboas@users.noreply.github.com>
2023-09-17 17:41:10 +0100

    current local

--- src/app.js
+++ src/app.js
@@ -155,3 +155,17 @@ fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 search("Porto");
 
+function searchLocation(position) {
+  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+// bonue feature week 5
+let currentButton = document.querySelector("#current-button");
+currentButton.addEventListener("click", getCurrentLocation);
